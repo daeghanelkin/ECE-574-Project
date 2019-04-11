cc3ba391d2db6ceba71ddea38d8197ab19000d0e
dpelican <daeghanelkin@gmail.com>
2019-04-11 18:25:07 -0400

    Now displays image as it's being combined

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC = gcc
 CFLAGS = -g -Wall
-LIBS = -lX11 -ljpeg
+LIBS = -lX11 -ljpeg -lm
 
 all: 
 	$(CC) -o sobel_before $(CFLAGS) sobel_before.c $(LIBS)
--- sobel_before.c
+++ sobel_before.c
@@ -14,6 +14,10 @@
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 
+/* Filters */
+static int sobel_x_filter[3][3]={{-1,0,+1},{-2,0,+2},{-1,0,+1}};
+static int sobel_y_filter[3][3]={{-1,-2,-1},{0,0,0},{1,2,+1}};
+
 /* Structure describing the image */
 struct image_t {
 	int x;
@@ -21,6 +25,118 @@ struct image_t {
 	int depth;	/* bytes */
 	unsigned char *pixels;
 };
+
+struct convolve_data_t {
+	struct image_t *old;
+	struct image_t *new;
+	int (*filter)[3][3];
+    int ystart;
+    int yend;
+};
+
+struct XImage_data_t {
+	XImage *img;
+	Display *dpy;
+	int screen;
+	Window win;
+};
+
+/* very inefficient convolve code */
+static void *generic_convolve(void *argument) {
+
+    int x,y,k,l,d;
+    uint32_t color;
+    int sum,depth,width;
+    
+    struct image_t *old;
+    struct image_t *new;
+    int (*filter)[3][3];
+    struct convolve_data_t *data;
+    int ystart, yend;
+    
+    /* Convert from void pointer to the actual data type */
+    data=(struct convolve_data_t *)argument;
+    old=data->old;
+    new=data->new;
+    filter=data->filter;
+    
+    ystart=data->ystart;
+    yend=data->yend;
+    
+    depth=old->depth;
+    width=old->x*old->depth;
+    
+    if (ystart==0) ystart=1;
+    if (yend==old->y) yend=old->y-1;
+    
+    for(d=0;d<3;d++) {
+       for(x=1;x<old->x-1;x++) {
+         for(y=ystart;y<yend;y++) {
+        sum=0;
+        for(k=-1;k<2;k++) {
+           for(l=-1;l<2;l++) {
+            color=old->pixels[((y+l)*width)+(x*depth+d+k*depth)];
+            sum+=color * (*filter)[k+1][l+1];
+           }
+        }
+    
+        if (sum<0) sum=0;
+        if (sum>255) sum=255;
+        
+        new->pixels[(y*width)+x*depth+d]=sum;
+         }
+       } 
+    }  
+    
+    return NULL;
+}
+
+
+static int combine(struct image_t *s_x,
+            struct image_t *s_y,
+            struct image_t *new,
+			struct XImage_data_t *image) {
+    int out;
+	int val;
+	int x,y;
+	int xsize = s_x->x;
+	int ysize = s_x->y;
+	int depth = s_x->depth;
+
+    for(y=0;y<ysize;y++) {
+		for(x=0; x < xsize; x++){
+			val = 0;
+			// Red 
+			out=sqrt((s_x->pixels[(y*xsize*depth)+x*depth]*s_x->pixels[(y*xsize*depth)+x*depth])
+				+(s_y->pixels[(y*xsize*depth)+x*depth]*s_y->pixels[(y*xsize*depth)+x*depth]));
+			if (out>255) out=255;
+			if (out<0) out=0;
+			new->pixels[(y*xsize*depth)+x*depth]=out;
+			val |= (out<<16);
+
+			// Green
+			out=sqrt((s_x->pixels[(y*xsize*depth)+x*depth+1]*s_x->pixels[(y*xsize*depth)+x*depth+1])
+				+(s_y->pixels[(y*xsize*depth)+x*depth+1]*s_y->pixels[(y*xsize*depth)+x*depth+1]));
+			if (out>255) out=255;
+			if (out<0) out=0;
+			new->pixels[(y*xsize*depth)+x*depth+1]=out;
+			val |= (out<<8);
+
+			// Blue
+			out=sqrt((s_x->pixels[(y*xsize*depth)+x*depth+2]*s_x->pixels[(y*xsize*depth)+x*depth+2])
+				+(s_y->pixels[(y*xsize*depth)+x*depth+2]*s_y->pixels[(y*xsize*depth)+x*depth+2]));
+			if (out>255) out=255;
+			if (out<0) out=0;
+			new->pixels[(y*xsize*depth)+x*depth+2]=out;
+			val |= (out);
+
+			XPutPixel(image->img, x, y, (long)val);
+			XPutImage(image->dpy,image->win,DefaultGC(image->dpy,image->screen),image->img,0,0,0,0,s_x->x,s_x->y);
+		}
+    }
+
+    return 0;
+}
 
 static int load_jpeg(char *filename, struct image_t *image) {
 
@@ -141,8 +257,10 @@ static int store_jpeg(char *filename, struct image_t *image) {
 
 int main(int argc, char **argv) {
 
-	struct image_t image;
-	int hex, x, y, d;
+	struct image_t image, sobel_x, sobel_y, new_image;
+	struct convolve_data_t sobel_data[2];
+	struct XImage_data_t x_data;
+	int hex, x, y;
 	int xsize, ysize, depth;
 
 	/* Check command line usage */
@@ -154,9 +272,41 @@ int main(int argc, char **argv) {
 	/* Load an image */
 	load_jpeg(argv[1],&image);
 
+	/* Allocate space for output image */
+	new_image.x=image.x;
+	new_image.y=image.y;
+	new_image.depth=image.depth;
+	new_image.pixels=malloc(image.x*image.y*image.depth*sizeof(char));
+
+	/* Allocate space for output image */
+	sobel_x.x=image.x;
+	sobel_x.y=image.y;
+	sobel_x.depth=image.depth;
+	sobel_x.pixels=malloc(image.x*image.y*image.depth*sizeof(char));
+
+	/* Allocate space for output image */
+	sobel_y.x=image.x;
+	sobel_y.y=image.y;
+	sobel_y.depth=image.depth;
+	sobel_y.pixels=malloc(image.x*image.y*image.depth*sizeof(char));
+
 	xsize = image.x;
 	ysize = image.y;
 	depth = image.depth;
+
+	sobel_data[0].old=&image;
+	sobel_data[0].new=&sobel_x;
+	sobel_data[0].filter=&sobel_x_filter;
+	sobel_data[0].ystart=0;
+	sobel_data[0].yend=image.y;
+	generic_convolve((void *)&sobel_data[0]);
+
+	sobel_data[1].old=&image;
+	sobel_data[1].new=&sobel_y;
+	sobel_data[1].filter=&sobel_y_filter;
+	sobel_data[1].ystart=0;
+	sobel_data[1].yend=image.y;
+	generic_convolve((void *)&sobel_data[1]);
 
 	Display *display = XOpenDisplay(NULL);
 	int screen_num = DefaultScreen(display);
@@ -167,36 +317,35 @@ int main(int argc, char **argv) {
 
 	XImage *img = XCreateImage(display,visual,DefaultDepth(display,screen_num),ZPixmap,0,data,xsize,ysize,32,0);
 
-	// X11 does 8bpp, 16bpp, and 32bpp. The 32bpp has format NGBR.
-	for(x = 0; x < xsize; x++) {
-		for(y = 0; y < ysize; y++) {
+	// X11 does 8bpp, 16bpp, and 32bpp. The 32bpp has format NBGR.
+	for(y = 0; y < ysize; y++) {
+		for(x = 0; x < xsize; x++) {
 			hex = 0;
-			// Blue
+			// Red 
 			hex |= (image.pixels[(y*xsize*depth)+x*depth]<<16);
 			// Green
 			hex |= (image.pixels[(y*xsize*depth)+x*depth+1]<<8);
-			// Red
+			// Blue
 			hex |= (image.pixels[(y*xsize*depth)+x*depth+2]);
 			XPutPixel(img, x, y, (long)hex);
 		}
 	}
-	
+
 	Window win = XCreateSimpleWindow(display,root,50,50,xsize,ysize,1,0,0);
 	XSelectInput(display,win,ExposureMask);
 	XMapWindow(display,win);
+	XPutImage(display,win,DefaultGC(display,screen_num),img,0,0,0,0,xsize,ysize);
 
-	XEvent event;
-	while(1)
-	{
-		XNextEvent(display,&event);
-		if(event.type == Expose)
-		{
-			XPutImage(display,win,DefaultGC(display,screen_num),img,0,0,0,0,xsize,ysize);
-		}
-	}
-	return 0;
+	x_data.dpy = display;
+	x_data.img = img;
+	x_data.screen = screen_num;
+	x_data.win = win;
 
-    store_jpeg("out.jpg",&image);
+	combine(&sobel_x, &sobel_y, &new_image, &x_data);
+
+	XDestroyImage(img);
+
+    store_jpeg("out.jpg",&new_image);
 
 	return 0;
 }
